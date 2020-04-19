e411737c0e4a0db5de7a4acd7a222dca83fb57c2
Ta-Chung Chen <chenalbert35@gmail.com>
2020-04-18 21:48:35 -0700

    Update mongoose connection

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-let MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
